ea6178c28106f0dc2b0ac41e66540f7e1d38a0fe
Thor-Ole Øby <punkthor@gmail.com>
2020-12-02 21:36:50 +0100

    Added models for album, genre and track.

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Album, Genre, Track
 
-# Register your models here.
+admin.site.register(Album)
+admin.site.register(Genre)
+admin.site.register(Track)
--- products/models.py
+++ products/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
-# Create your models here.
+
+class Genre(models.Model):
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_fiendly_name(self):
+        return self.friendly_name
+
+
+class Album(models.Model):
+    genre = models.ForeignKey('Genre', null=True, blank=True, on_delete=models.SET_NULL)
+    title = models.CharField(max_length=254)
+    sku = models.CharField(max_length=254, null=True, blank=True)
+    artist = models.CharField(max_length=254)
+    release_year = models.IntegerField()
+    era = models.IntegerField()
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    special_edition = models.BooleanField(default=False, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Track(models.Model):
+    album = models.ForeignKey('Album', null=True, blank=True, on_delete=models.SET_NULL)
+    title = models.CharField(max_length=254)
+
+    def __str__(self):
+        return self.title
