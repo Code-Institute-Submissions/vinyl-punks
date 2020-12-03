d3e3bda75af704cfd9e18c2836325e75f65cc6e2
Thor-Ole Øby <punkthor@gmail.com>
2020-12-03 14:41:22 +0100

    Customized admin

--- products/admin.py
+++ products/admin.py
@@ -1,6 +1,34 @@
 from django.contrib import admin
 from .models import Album, Genre, Track
 
-admin.site.register(Album)
-admin.site.register(Genre)
-admin.site.register(Track)
+
+class AlbumAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'artist',
+        'genre',
+        'release_year',
+        'price',
+        'special_edition',
+    )
+
+    ordering = ('title',)
+
+
+class TrackAdmin(admin.ModelAdmin):
+    list_display = (
+        'album',
+        'title',
+    )
+
+
+class GenreAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name'
+    )
+
+
+admin.site.register(Album, AlbumAdmin)
+admin.site.register(Genre, GenreAdmin)
+admin.site.register(Track, TrackAdmin)
