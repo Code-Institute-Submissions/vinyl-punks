99340c2c2dd18cd4cab8a745473aeadb7ec45f21
Thor-Ole Øby <punkthor@gmail.com>
2020-12-04 17:54:36 +0100

    Added product details view

--- products/views.py
+++ products/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse, get_object_or_404
 from .models import Album, Track
 
 
@@ -6,7 +6,6 @@ def all_products(request):
     """ A view to display all albums """
 
     albums = Album.objects.all()
-    print(albums)
     tracks = Track.objects.all()
 
     context = {
@@ -15,3 +14,18 @@ def all_products(request):
     }
 
     return render(request, 'products/products.html', context)
+
+
+def product_details(request, product_id):
+    """ A view to display product details """
+
+    album = get_object_or_404(Album, pk=product_id)
+    tracks = Track.objects.filter(album=album)
+    print(tracks)
+
+    context = {
+        'album': album,
+        'tracks': tracks,
+    }
+
+    return render(request, 'products/product_details.html', context)
