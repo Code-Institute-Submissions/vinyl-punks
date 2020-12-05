de5a9765a0a7c2ded02734b45d2ecd26a4d3d465
Thor-Ole Øby <punkthor@gmail.com>
2020-12-05 13:39:59 +0100

    Added functionality to handle get requests via nav links

--- products/views.py
+++ products/views.py
@@ -1,16 +1,31 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
-from .models import Album, Track
+from .models import Album, Track, Genre
 from django.db.models import Q
 
 
 def all_products(request):
     """ A view to display all albums """
     query = None
-
+    genre = None
+    era = None
+    special_edition = False
     albums = Album.objects.all()
     tracks = Track.objects.all()
 
     if request.GET:
+        if 'genre' in request.GET:
+            genre = request.GET['genre']
+            albums = albums.filter(genre__name=genre)
+
+        if 'era' in request.GET:
+            era = request.GET['era']
+            albums = albums.filter(era=era)
+
+        if 'special_edition' in request.GET:
+            print(special_edition)
+            special_edition = request.GET['special_edition']
+            albums = albums.filter(special_edition=special_edition)
+
         if 'search_query' in request.GET:
             query = request.GET['search_query']
             if not query:
