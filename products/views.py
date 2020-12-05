99866821cf6d41a1faae1c073e11b3d875fd2680
Thor-Ole Øby <punkthor@gmail.com>
2020-12-05 12:12:43 +0100

    Added search functionality

--- products/views.py
+++ products/views.py
@@ -1,16 +1,28 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from .models import Album, Track
+from django.db.models import Q
 
 
 def all_products(request):
     """ A view to display all albums """
+    query = None
 
     albums = Album.objects.all()
     tracks = Track.objects.all()
 
+    if request.GET:
+        if 'search_query' in request.GET:
+            query = request.GET['search_query']
+            if not query:
+                return redirect(reverse('products'))
+
+            queries = Q(title__icontains=query) | Q(artist__icontains=query)
+            albums = albums.filter(queries)
+
     context = {
         'albums': albums,
         'tracks': tracks,
+        'query': query
     }
 
     return render(request, 'products/products.html', context)
@@ -21,7 +33,6 @@ def product_details(request, product_id):
 
     album = get_object_or_404(Album, pk=product_id)
     tracks = Track.objects.filter(album=album)
-    print(tracks)
 
     context = {
         'album': album,
