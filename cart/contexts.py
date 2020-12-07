87af7190b9ef2275f57419f0008ade1744edc7e8
Thor-Ole Øby <punkthor@gmail.com>
2020-12-07 20:05:13 +0100

    Added loop for iterating through bag items.

--- cart/contexts.py
+++ cart/contexts.py
@@ -1,5 +1,7 @@
 from decimal import Decimal
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from products.models import Album
 
 
 def cart_contents(request):
@@ -7,12 +9,24 @@ def cart_contents(request):
     cart_items = []
     total = 0
     product_count = 0
+    cart = request.session.get('cart', {})
+    print(cart.items())
+
+    for item_id, quantity in cart.items():
+        product = get_object_or_404(Album, pk=item_id)
+        total += quantity * product.price
+        product_count += quantity
+        cart_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'product': product,
+        })
 
     delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
     grand_total = delivery + total
 
     context = {
-        'cart-items': cart_items,
+        'cart_items': cart_items,
         'total': total,
         'product_count': product_count,
         'delivery': delivery,
