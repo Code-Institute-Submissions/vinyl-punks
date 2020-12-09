f27837d73d45d3fc3d86f521987511dcfd28a959
Thor-Ole Øby <punkthor@gmail.com>
2020-12-09 18:56:31 +0100

    Added path to delete from cart view.

--- cart/urls.py
+++ cart/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.view_cart, name='view_cart'),
     path('add/<item_id>/', views.add_to_cart, name='add_to_cart'),
     path('update_cart/<item_id>/', views.update_cart, name='update_cart'),
+    path('delete_from_cart/<item_id>/', views.delete_from_cart, name='delete_from_cart'),
 ]
