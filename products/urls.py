75951b4cfed315c2fe7d31ac7cf54cc089f2c7d5
Thor-Ole Øby <punkthor@gmail.com>
2020-12-10 21:34:52 +0100

    Added paths for delete from cart and contexts

--- products/urls.py
+++ products/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from . import views
+from cart import contexts
+from cart.contexts import cart_contents
 
 urlpatterns = [
     path('', views.all_products, name='products'),
-    path('<int:product_id>', views.product_details, name='product_details')
+    path('<int:product_id>', views.product_details, name='product_details'),
+    path('delete_from_cart/<item_id>/', views.delete_from_cart, name='delete_from_cart'),
+    path('cart_contents/', contexts.cart_contents, name='cart_contents'),
 ]
