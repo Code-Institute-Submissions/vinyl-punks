88dddf53dfa2e94eb2f84dd1b2430594f58fdbdb
Thor-Ole Øby <punkthor@gmail.com>
2020-12-10 21:38:48 +0100

    Cleaned up white space

--- products/views.py
+++ products/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from .models import Album, Track, Genre
 from django.db.models import Q
-from django.db.models.functions import Lower
+from cart.views import delete_from_cart
 
 
 def all_products(request):
