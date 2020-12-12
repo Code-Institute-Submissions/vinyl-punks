0644f55b54dfb05ee24c2e10149bd16d7b6c7ff5
Thor-Ole Øby <punkthor@gmail.com>
2020-12-12 20:15:57 +0100

    Created lineitems model and model functions

--- checkout/models.py
+++ checkout/models.py
@@ -1,10 +1,14 @@
+import uuid
 from django.db import models
-form django_countries.fields import CountryField
+from django.db.models import Sum
+from django.conf import settings
+
+from django_countries.fields import CountryField
+from products.models import Album
 
 
 class Order(models.Model):
 
-   class Order(models.Model):
     order_number = models.CharField(max_length=32, null=False, editable=False)
     full_name = models.CharField(max_length=50, null=False, blank=False)
     email = models.EmailField(max_length=254, null=False, blank=False)
@@ -23,10 +27,37 @@ class Order(models.Model):
     grand_total = models.DecimalField(max_digits=10, decimal_places=2,
                                       null=False, default=0)
 
+    def _generate_order_number(self):
+        """ Generate a unique order number """
+        return uuid.uuid4().hex.upper()
+
+    def update_total(self):
+        """ Update grand total for each item added """
+        self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum']
+        self.delivery_cost = self.order_total * settings.STANDARD_DELIVERY_PERCENTAGE /100
+        self.grand_total = self.delivery_cost + self.order_total
+        self.save()
+
+    def save(self, *args, **kwargs):
+        """ Override original save to set order number if it doesn't exist"""
+        if not self.order_number:
+            self.order_number = self._generate_order_number()
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.order_number
+
 
 class OrderLineItem(models.Model):
     order = models.ForeignKey(Order, null=False, blank=False, on_delete=models.CASCADE, related_name='lineitems')
-    product = models.ForeignKey(Product, null=False, blank=False, on_delete=models.CASCADE)
-    product_size = models.CharField(max_length=2, null=True, blank=True) # XS, S, M, L, XL
+    product = models.ForeignKey(Album, null=False, blank=False, on_delete=models.CASCADE)
     quantity = models.IntegerField(null=False, blank=False, default=0)
     lineitem_total = models.DecimalField(max_digits=6, decimal_places=2, null=False, blank=False, editable=False)
+
+    def save(self, *args, **kwargs):
+        """ Override original save to set order number if it doesn't exist"""
+        self.lineitem_total = self.price * self.quantity
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f'SKU {self.product.sku} on {self.order.order_number}'
