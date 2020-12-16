d85ec92033914d09888acf45587259eb5f340331
Thor-Ole Øby <punkthor@gmail.com>
2020-12-16 09:24:47 +0100

    Added boilerplate from stripe and modified.

--- checkout/static/checkout/js/stripe.js
+++ checkout/static/checkout/js/stripe.js
@@ -4,30 +4,60 @@ let stripe = Stripe(stripePublicKey);
 let elements = stripe.elements();
 let style = {
     base: {
-      color: '#32325d',
-      fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
-      fontSmoothing: 'antialiased',
-      fontSize: '16px',
-      '::placeholder': {
-        color: '#aab7c4'
-      }
+        color: '#32325d',
+        fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
+        fontSmoothing: 'antialiased',
+        fontSize: '16px',
+        '::placeholder': {
+            color: '#aab7c4'
+        }
     },
     invalid: {
-      color: '#fa755a',
-      iconColor: '#fa755a'
+        color: '#fa755a',
+        iconColor: '#fa755a'
     }
-  };
-let card = elements.create("card", {style: style});
+};
+let card = elements.create("card", { style: style });
 
 card.mount("#card-field");
 
-card.addEventListener("change", function(e) { 
+card.addEventListener("change", function (e) {
     let errorContainer = $("#card-error");
     if (e.error) {
         let html = `<i class="fas fa-exclamation-circle"></i>
                     <span>${e.error.message}`;
-        $(errorContainer).append(html)
+        $(errorContainer).append(html);
     } else {
         errorContainer.text("")
     }
 });
+
+var form = document.getElementById("payment-form");
+form.addEventListener("submit", function (event) {
+    event.preventDefault();
+    card.update({ "disabled": true });
+    $("#submit-btn").attr("disabled", true)
+    // Complete payment when the submit button is clicked
+    payWithCard(stripe, card, clientSecret);
+});
+
+var payWithCard = function (stripe, card, clientSecret) {
+
+    stripe.confirmCardPayment(clientSecret, {
+        payment_method: {
+            card: card
+        }
+    }).then(function (result) {
+        let errorContainer = $("#card-error");
+        if (result.error) {
+            let html = `<i class="fas fa-exclamation-circle"></i>
+            <span>${e.error.message}`;
+            $(errorContainer).append(html);
+            card.update({ "disabled": false });
+            $("#submit-button").attr("disabled", false)
+        } else {
+            form.submit();
+        }
+    });
+};
+
