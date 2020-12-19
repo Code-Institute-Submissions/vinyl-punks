6f6c51508535c06c4d1657c38a3033ddfd010ab1
Thor-Ole Øby <punkthor@gmail.com>
2020-12-19 14:29:48 +0100

    Added shipping and billing details to payment intent, as well as posting user information to cache checkout data view.

--- checkout/static/checkout/js/stripe.js
+++ checkout/static/checkout/js/stripe.js
@@ -39,15 +39,52 @@ form.addEventListener("submit", function (event) {
     $("#submit-btn").attr("disabled", true)
     $(form).fadeToggle(100);
     $("#loading-overlay").fadeToggle(100);
-    payWithCard(stripe, card, clientSecret);
+    var saveInfo = Boolean($("#id-save-info").attr('checked'));
+    // From using {% csrf_token %} in the form
+    var csrfToken = $('input[name="csrfmiddlewaretoken"]').val();
+    var postData = {
+        'csrfmiddlewaretoken': csrfToken,
+        'client_secret': clientSecret,
+        'save_info': saveInfo,
+    };
+    var url = '/checkout/cache_checkout_data/'
+    $.post(url, postData).done(function () {
+        payWithCard(stripe, card, clientSecret);
+    }).fail(function () {
+        location.reload();
+    })
 });
 
 var payWithCard = function (stripe, card, clientSecret) {
 
     stripe.confirmCardPayment(clientSecret, {
         payment_method: {
-            card: card
-        }
+            card: card,
+            billing_details: {
+                name: $.trim(form.full_name.value),
+                phone: $.trim(form.phone_number.value),
+                email: $.trim(form.email.value),
+                address: {
+                    line1: $.trim(form.street_address1.value),
+                    line2: $.trim(form.street_address2.value),
+                    city: $.trim(form.town_or_city.value),
+                    country: $.trim(form.country.value),
+                    state: $.trim(form.county.value),
+                }
+            }
+        },
+        shipping: {
+            name: $.trim(form.full_name.value),
+            phone: $.trim(form.phone_number.value),
+            address: {
+                line1: $.trim(form.street_address1.value),
+                line2: $.trim(form.street_address2.value),
+                city: $.trim(form.town_or_city.value),
+                country: $.trim(form.country.value),
+                postal_code: $.trim(form.postcode.value),
+                state: $.trim(form.county.value),
+            }
+        },
     }).then(function (result) {
         if (result.error) {
             let errorContainer = $("#card-error");
