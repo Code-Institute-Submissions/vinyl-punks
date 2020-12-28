db69d736f47dd05418d7d137e973e4afd5431f85
Thor-Ole Øby <punkthor@gmail.com>
2020-12-28 17:46:18 +0100

    Created form for adding product

--- products/forms.py
+++ products/forms.py
@@ -11,5 +11,6 @@ class ProductForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         genres = Genre.objects.all()
+        genre_names = [(g.id, g.get_friendly_name()) for g in genres]
 
-        self.fields['genre'].choices = genres
+        self.fields['genre'].choices = genre_names
