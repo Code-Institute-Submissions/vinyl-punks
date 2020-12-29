18c8c773e4f06e2ae029391b6ba14da9151bfc97
Thor-Ole Øby <punkthor@gmail.com>
2020-12-29 10:30:11 +0100

    Added view for deleting product

--- products/views.py
+++ products/views.py
@@ -125,4 +125,17 @@ def edit_product(request, product_id):
         'product': product,
     }
 
-    return render(request, template, context)    
+    return render(request, template, context)
+
+
+@login_required
+def delete_product(request, product_id):
+    """Delete a product from the store"""
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only store owners can do that.')
+        return redirect(reverse('products'))
+
+    product = get_object_or_404(Album, pk=product_id)
+    product.delete()
+    messages.success(request, 'Product deleted!')
+    return redirect(reverse('products'))
