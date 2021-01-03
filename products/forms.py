2b5a5c4eeeb2a74bebfcc82891e3e043d03f384c
Thor-Ole Øby <punkthor@gmail.com>
2021-01-03 16:56:42 +0100

    Added form fro writing a review.

--- products/forms.py
+++ products/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Album, Genre
+from .models import Album, Genre, Review
 
 
 class ProductForm(forms.ModelForm):
@@ -14,3 +14,26 @@ class ProductForm(forms.ModelForm):
         genre_names = [(g.id, g.get_friendly_name()) for g in genres]
 
         self.fields['genre'].choices = genre_names
+
+
+class ReviewForm(forms.ModelForm):
+
+    class Meta:
+        model = Review
+        exclude = ('album', 'author', 'created_on',)
+
+    def __init__(self, *args, **kwargs):
+        """
+        Add placeholders and classes, remove auto-generated
+        labels.
+        """
+
+        super().__init__(*args, **kwargs)
+        placeholders = {
+            'content': 'Write a review..',
+        }
+
+        for field in self.fields:
+            placeholder = placeholders[field]
+            self.fields[field].widget.attrs['placeholder'] = placeholder
+            self.fields[field].label = False
