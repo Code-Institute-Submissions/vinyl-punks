b98c051823bec325e5c3b5f6962147264ef18491
Thor-Ole Øby <punkthor@gmail.com>
2021-01-13 20:58:29 +0100

    Updated url to accept second variable

--- products/urls.py
+++ products/urls.py
@@ -12,7 +12,7 @@ urlpatterns = [
     path('edit/<int:product_id>/', views.edit_product, name='edit_product'),
     path('delete/<int:product_id>/', views.delete_product, name='delete_product'),
     path('add_review/<int:product_id>/', views.add_review, name='add_review'),
-    path('update_review/<int:product_id>/', views.update_review, name='update_review'),
+    path('update_review/<int:review_id>/<int:rating_id>/', views.update_review, name='update_review'),
     path('add_tracks/', views.add_tracks, name='add_tracks'),
     path('delete_track/<int:track_id>/', views.delete_track, name='delete_track'),
 ]
